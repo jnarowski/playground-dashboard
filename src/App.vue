2be5aaa7efbda07edf95af9e7e884c1d66bae90d
jnarowski <jnarowski@gmail.com>
2019-08-09 12:07:44 -0600

    the dashboard for my life

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+html {
+  background-color: #1c2532;
+  color: #fff;
+  text-align: center;
+  word-spacing: 1px;
+  font-size: 16px;
+  font-family: Heebo, Helvetica Neue, Source Sans Pro, Helvetica, Arial,
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+  -ms-text-size-adjust: 100%;
+}
+</style>
