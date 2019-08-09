4079b50574f6b9ea44697b33ca7a14ff61d9094a
jnarowski <jnarowski@gmail.com>
2019-08-09 12:21:30 -0600

    added express

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+const app = express();
 const port = process.env.PORT || 80;
-app.listen(port);
+
+app.use(serveStatic(path.join(__dirname, "dist")))
+app.listen(port)
